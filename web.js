0c57e5894c3f55abce7ea98270865122aa4db856
Tibor Dumitriu <tdumitriu@gmail.com>
2013-07-10 00:04:36 +0000

    Reading from file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
 var express = require('express');
+
+var fileName = "index.html";
+var buffer = new Buffer(fs.readFileSync(fileName));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
