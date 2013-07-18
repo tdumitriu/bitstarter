2eb5214c23367f34ada26147c240c986c37fecd8
Tibor Dumitriu <tdumitriu@gmail.com>
2013-07-18 04:09:21 +0000

    Added css and js to the index.html

--- grader.js
+++ grader.js
@@ -21,11 +21,14 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
 var fs = require('fs');
+var rest = require('restler');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://young-falls-7639.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +58,31 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var buildfn = function(checksfile) {
+    var response2me = function(data, response) {
+        if (data instanceof Error) {
+            console.log("%s does not exist. Exiting.", url);
+            process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+        } else {
+            $ = cheerio.load(data);
+            var checks = loadChecks(checksfile).sort();
+            var out = {};
+            for(var ii in checks) {
+              var present = $(checks[ii]).length > 0;
+              out[checks[ii]] = present;
+            }
+            var outJson = JSON.stringify(out, null, 4);
+            console.log(outJson);
+        }
+    };
+    return response2me;
+};
+
+var checkHtmlFileFromUrl = function(url, checksfile) {
+    var response2me = buildfn(checksfile);
+    rest.get(url).on('complete', response2me);
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -64,11 +92,9 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_address>', 'Path to remote address')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+      checkHtmlFileFromUrl(program.url, program.checks)
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
